b0c672bb56fbf55ead3899bf33ccff31d13a3896
songhuaixu <965991028@qq.com>
2024-04-16 18:20:39 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -18,7 +18,7 @@ fn main() {
     let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
 
     match (target_os.as_str(), target_env.as_str()) {
-        ("linux", _) | ("windows", "gnu") => println!("cargo:rustc-link-lib=dylib=stdc++"),
+        ("linux", _) | ("windows", "gnu") |  ("android", _)  => println!("cargo:rustc-link-lib=dylib=stdc++"),
         ("macos", _) | ("ios", _) => println!("cargo:rustc-link-lib=dylib=c++"),
         ("windows", "msvc") => {}
         _ => unimplemented!(
